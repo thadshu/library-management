2789ef436ce18df19da217f1c2fc324921ac98f4
thadshu <87629720+thadshu@users.noreply.github.com>
2023-10-28 16:38:35 +0530

    Add files via upload

---
+++ bookrequestapprove.php
@@ -0,0 +1,39 @@
+<?php
+session_start();
+
+$adminid=$_SESSION["adminid"];
+?>
+
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <![endif]-->
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Admin Dashboard</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+      <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+      <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <!-- <link rel="stylesheet" href="style.css"> -->
+    </head>
+    <style>
+        .rightinnerdiv {
+    float: right;
+    width: 75%;
+}
+    </style>
+    <body>
+        <h1>hello</h1>
+    <div class="rightinnerdiv">   
+            <div id="bookrequestapprove" class="innerright portion" style="">
+            <Button class="greenbtn" >BOOK REQUEST APPROVE</Button>
+</div>
+</div>
+    </body>
+    </html>
